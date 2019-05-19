a68954c07739694f5017a06dee48a338387d2451
janeluck <janeluck158@gmail.com>
2019-05-19 17:42:02 +0800

    insert many

--- index.js
+++ index.js
@@ -11,41 +11,54 @@ const dbName = 'robot';
 // Create a new MongoClient
 const client = new MongoClient(url);
 req.on('error', function (error) {
-    // handle any request errors
+  // handle any request errors
 });
 
 req.on('response', function (res) {
-    var stream = this; // `this` is `req`, which is a stream
+  var stream = this; // `this` is `req`, which is a stream
 
-    if (res.statusCode !== 200) {
-        this.emit('error', new Error('Bad status code'));
-    } else {
-        stream.pipe(feedparser);
-    }
+  if (res.statusCode !== 200) {
+    this.emit('error', new Error('Bad status code'));
+  } else {
+    stream.pipe(feedparser);
+  }
 });
 
 feedparser.on('error', function (error) {
-    // always handle errors
+  // always handle errors
 });
 
 feedparser.on('readable', function () {
-    // This is where the action is!
-    var stream = this; // `this` is `feedparser`, which is a stream
-    var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-    var item;
+  // This is where the action is!
+  var stream = this; // `this` is `feedparser`, which is a stream
+  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+  var item;
 
-    while (item = stream.read()) {
-        //console.log(item);
-    }
+  while (item = stream.read()) {
+    //console.log(item);
+  }
 });
 
 
 // Use connect method to connect to the Server
 client.connect(function (err) {
 
-    const db = client.db(dbName);
-    console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  console.log("Connected successfully to server");
+  db.collection("news").insertMany([{
+    theme: "my mongodb000",
+    author: "jack",
+  },
+    {
+      theme: "my mongodb001",
+      author: "jack",
+    },
+  ], function (err, res) {
+    if (err) throw err;
+    console.log("文档插入成功");
     client.close();
+  });
+
 });
 
 
